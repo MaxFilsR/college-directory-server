eea4b911d66c96e0ec5600fc13bf1641761ce8cf
boukhadija <128822503+boukhadija@users.noreply.github.com>
2025-11-30 21:13:44 -0500

    added student model

--- database/models/Campus.js
+++ database/models/Campus.js
@@ -5,6 +5,7 @@ It defines the campus model for the database.
 ==================================================*/
 const Sequelize = require('sequelize');  // Import Sequelize
 const db = require('../db');  // Import Sequelize database instance called "db"
+const campus_image_url='https://images.pexels.com/photos/207692/pexels-photo-207692.jpeg?_gl=1*1ltgmlh*_ga*ODEzMjI4NDg5LjE3NjQxOTg4MjU.*_ga_8JE65Q40S6*czE3NjQxOTg4MjUkbzEkZzEkdDE3NjQxOTk5NzgkajQwJGwwJGgw';
 
 // Define the campus model
 const Campus = db.define("campus", {
@@ -20,6 +21,11 @@ const Campus = db.define("campus", {
 
   description: {
     type: Sequelize.STRING,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: campus_image_url,
+    allowNull: true
   }
 });
 
--- database/models/Student.js
+++ database/models/Student.js
@@ -5,6 +5,7 @@ It defines the student model for the database.
 ==================================================*/
 const Sequelize = require('sequelize');  // Import Sequelize
 const db = require('../db');  // Import Sequelize database instance called "db"
+const student_image_url='https://images.pexels.com/photos/20230197/pexels-photo-20230197.jpeg?_gl=1*hdwxx6*_ga*ODEzMjI4NDg5LjE3NjQxOTg4MjU.*_ga_8JE65Q40S6*czE3NjQxOTg4MjUkbzEkZzEkdDE3NjQxOTg5MTIkajM2JGwwJGgw';
 
 const Student = db.define("student", {
   firstname: {
@@ -15,6 +16,27 @@ const Student = db.define("student", {
   lastname: {
     type: Sequelize.STRING,
     allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    // unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: student_image_url,
+    allowNull: true
+  },
+  gpa: {
+    type: Sequelize.DECIMAL(10, 2),
+    allowNull: true,
+    validate: {
+      min: 0.0,
+      max: 4.0
+    }
   }
 });
 
