c0571ffc416f2c5cdda9a23a8d7049fc6c4ed54a
MaxFilsR <maxfilsr@gmail.com>
2025-12-06 21:28:43 -0500

    fixed type in student first name and last name

--- database/models/Student.js
+++ database/models/Student.js
@@ -3,25 +3,26 @@
 
 It defines the student model for the database.
 ==================================================*/
-const Sequelize = require('sequelize');  // Import Sequelize
-const db = require('../db');  // Import Sequelize database instance called "db"
-const student_image_url='https://images.pexels.com/photos/20230197/pexels-photo-20230197.jpeg?_gl=1*hdwxx6*_ga*ODEzMjI4NDg5LjE3NjQxOTg4MjU.*_ga_8JE65Q40S6*czE3NjQxOTg4MjUkbzEkZzEkdDE3NjQxOTg5MTIkajM2JGwwJGgw';
+const Sequelize = require("sequelize"); // Import Sequelize
+const db = require("../db"); // Import Sequelize database instance called "db"
+const student_image_url =
+  "https://images.pexels.com/photos/20230197/pexels-photo-20230197.jpeg?_gl=1*hdwxx6*_ga*ODEzMjI4NDg5LjE3NjQxOTg4MjU.*_ga_8JE65Q40S6*czE3NjQxOTg4MjUkbzEkZzEkdDE3NjQxOTg5MTIkajM2JGwwJGgw";
 
 const Student = db.define("student", {
-  firstname: {
+  firstName: {
     type: Sequelize.STRING,
     allowNull: false,
-    notEmpty:{
-      msg:"First name cannot be empty"
-    }
+    notEmpty: {
+      msg: "First name cannot be empty",
+    },
   },
 
-  lastname: {
+  lastName: {
     type: Sequelize.STRING,
     allowNull: false,
-    notEmpty:{
-      msg:"Last name cannot be empty"
-    }
+    notEmpty: {
+      msg: "Last name cannot be empty",
+    },
   },
   email: {
     type: Sequelize.STRING,
@@ -29,24 +30,24 @@ const Student = db.define("student", {
     // unique: true,
     validate: {
       isEmail: true,
-      notEmpty:{
-        msg:"Email cannot be empty"
+      notEmpty: {
+        msg: "Email cannot be empty",
       },
-    }
+    },
   },
   imageUrl: {
     type: Sequelize.STRING,
     defaultValue: student_image_url,
-    allowNull: true
+    allowNull: true,
   },
   gpa: {
     type: Sequelize.DECIMAL(10, 2),
     allowNull: true,
     validate: {
       min: 0.0,
-      max: 4.0
-    }
-  }
+      max: 4.0,
+    },
+  },
 });
 
 // Export the student model
